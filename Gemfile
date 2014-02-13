786f8129b89cdda14d32bcbd64f9b0d30d979f3e
Edmund Haselwanter <edmund@haselwanter.com>
2014-02-13 13:31:37 +0100

    get the specs running. we need to add the gems used with chef_gem and we need a Chef::Config for the delete_validation key

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ gem "chefspec",   "~> 3.0"
 gem "foodcritic", "~> 3.0"
 gem "rake"
 gem "rubocop"
+# add gems for the spec tests otherwise it will fail to require
+# https://github.com/sethvargo/chefspec/issues/336
+gem "chef-sugar"
+gem "chef-handler-updated-resources"
 
 group :integration do
   gem "test-kitchen"
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,13 +2,13 @@ require "spec_helper"
 
 # Write unit tests with ChefSpec - https://github.com/sethvargo/chefspec#readme
 describe "base::default" do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::Runner.new do
+    # nedded for the chef-client::delete_validation
+    Chef::Config[:client_key] = "a dummy client"
+  end.converge(described_recipe) }
 
   it "should include apt" do
     expect(chef_run).to include_recipe "apt"
   end
 
-  it "does something" do
-    pending "Replace this with meaningful tests"
-  end
 end
