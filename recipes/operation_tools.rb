e8445c4fdeefad4da7f3f9994b1c3199503f36b3
Edmund Haselwanter <edmund@haselwanter.com>
2014-02-13 18:04:34 +0100

    nasty re-run bug not captured with chefspec et.al

--- recipes/operation_tools.rb
+++ recipes/operation_tools.rb
@@ -8,6 +8,6 @@ end
 
 motd_message = "\ntry one of the operation-tools: #{node['base']['operation-tools'].join(', ')}\n"
 
-if node['base']['motd']['additional_text'] !~ motd_message
+if node['base']['motd']['additional_text'] !~ /#{motd_message}/
   node.set['base']['motd']['additional_text'] = (node['base']['motd']['additional_text'] || "") + motd_message
 end
