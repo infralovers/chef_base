a1c5051353afce65ae53c129fef7b19ce9ebf0cd
Edmund Haselwanter <edmund@haselwanter.com>
2014-02-13 15:02:45 +0100

    cs fix

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,10 +2,12 @@ require "spec_helper"
 
 # Write unit tests with ChefSpec - https://github.com/sethvargo/chefspec#readme
 describe "base::default" do
-  let(:chef_run) { ChefSpec::Runner.new do
-    # nedded for the chef-client::delete_validation
-    Chef::Config[:client_key] = "a dummy client"
-  end.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::Runner.new do
+      # nedded for the chef-client::delete_validation
+      Chef::Config[:client_key] = "a dummy client"
+    end.converge(described_recipe)
+  end
 
   it "should include apt" do
     expect(chef_run).to include_recipe "apt"
