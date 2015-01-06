f12ebb59449a177cd6c56f3b307c2a0c5c3b6a10
Edmund Haselwanter <me@ehaselwanter.com>
2015-01-06 20:35:17 +0100

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,16 @@
 name 'base'
 maintainer 'Edmund Haselwanter'
 maintainer_email 'me@ehaselwanter.com'
-license 'All rights reserved'
+license 'Apache v2.0'
+
 description 'Installs/Configures chef_base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+source_url 'https://github.com/iteh/chef_base'
+
 version '0.4.0'
 
-supports 'ubuntu'
+supports 'ubuntu' , ">= 12.04"
 
 depends 'apt'
 depends 'build-essential'
